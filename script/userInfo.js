2863766c9cd61f852b45b568998c212ee6c5d2d0
Valentin <7705678@gmail.com>
2022-04-22 22:17:44 +0300

    feat: profile card overview

--- script/userInfo.js
+++ script/userInfo.js
@@ -6,7 +6,7 @@ export const User = {
   birthday: '01.01.1990',
   location: 'г. Санкт-Петербург',
   nickname: 'Белка',
-  photo: '../images/profile/belka.svg',
+  photo: 'images/profile/belka.svg',
   phone: '+7 123 456 78 90',
   email: 'anna@liza-alert.ru',
   password: 'qwerty'
---
+++ script/vars-profile.js
@@ -0,0 +1,6 @@
+export const userAvatar = document.querySelector('.account-overview__photo');
+export const userName = document.querySelector('.account-overview__title');
+export const userLevel = document.querySelector('.account-overview__user-level');
+export const userProf = document.querySelector('.account-overview__user-profession');
+export const userCountCourses = document.querySelector('.account-overview__user-courses');
+
