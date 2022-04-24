3cfcf1db63c715316dfddd2558855af439eb09b1
Evgeny Pulnikov <sklve52@outlook.com>
2022-04-24 16:38:07 +0300

    Revert "Merge pull request #20 from evgenypulnikov/feature/data-array"
    This reverts commit cbfdcd4585c9ad09542612c10fe541c4ecfcbcc8, reversing
    changes made to ef930ba55dcb9c80cd3f1b6daf120b1e129bfcff.

--- script/userInfo.js
+++ script/userInfo.js
@@ -3,11 +3,11 @@ export const user = {
   level: 'Профессионал',
   profession: 'Картограф',
   courses: 2,
-  birthday: '1990-01-01',
+  birthday: '01.01.1990',
   location: 'г. Санкт-Петербург',
   nickname: 'Белка',
   photo: 'images/profile/belka.svg',
-  phone: '+71234567890',
+  phone: '+7 123 456 78 90',
   email: 'anna@liza-alert.ru',
   password: 'qwerty'
 }
--- script/vars-profile.js
+++ script/vars-profile.js
@@ -3,17 +3,3 @@ export const userName = document.querySelector('.card__title');
 export const userLevel = document.querySelector('.card__user-level');
 export const userProf = document.querySelector('.card__user-profession');
 export const userCountCourses = document.querySelector('.card__user-courses');
-
-// User data
-
-export const formUserData = document.querySelector('form[name="userData"]'),
-             inputName = formUserData.querySelector('input[name="name"]'),
-             inputBirthday = formUserData.querySelector('input[name="birthday"]'),
-             inputLocation = formUserData.querySelector('input[name="location"]'),
-             inputNickname = formUserData.querySelector('input[name="nickname"]'),
-             inputPhoto = formUserData.querySelector('input[name="photo"]');
-
-export const formUserAccount = document.querySelector('form[name="userAccount"]'),
-             inputPhone = formUserAccount.querySelector('input[name="phone"]'),
-             inputEmail = formUserAccount.querySelector('input[name="email"]'),
-             inputPassword = formUserAccount.querySelector('input[name="password"]');
