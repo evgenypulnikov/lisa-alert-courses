6fea040e53493296d35aa60ce7a5454ed5d8f15f
Valentin <7705678@gmail.com>
2022-04-27 17:06:44 +0300

    feat: edit user data

--- script/vars-profile.js
+++ script/vars-profile.js
@@ -1,5 +1,17 @@
-export const userAvatar = document.querySelector('.account-overview__photo');
-export const userName = document.querySelector('.account-overview__title');
-export const userLevel = document.querySelector('.account-overview__user-level');
-export const userProf = document.querySelector('.account-overview__user-profession');
-export const userCountCourses = document.querySelector('.account-overview__user-courses');
+export const userAvatar = document.querySelector('.card__photo');
+export const userName = document.querySelector('.card__title');
+export const userLevel = document.querySelector('.card__user-level');
+export const userProf = document.querySelector('.card__user-profession');
+export const userCountCourses = document.querySelector('.card__user-courses');
+
+export const formUserData = document.querySelector('form[name="userData"]'),
+             inputName = formUserData.querySelector('input[name="name"]'),
+             inputBirthday = formUserData.querySelector('input[name="birthday"]'),
+             inputLocation = formUserData.querySelector('input[name="location"]'),
+             inputNickname = formUserData.querySelector('input[name="nickname"]'),
+             inputPhoto = formUserData.querySelector('input[name="photo"]');
+
+export const formUserAccount = document.querySelector('form[name="userAccount"]'),
+             inputPhone = formUserAccount.querySelector('input[name="phone"]'),
+             inputEmail = formUserAccount.querySelector('input[name="email"]'),
+             inputPassword = formUserAccount.querySelector('input[name="password"]');
